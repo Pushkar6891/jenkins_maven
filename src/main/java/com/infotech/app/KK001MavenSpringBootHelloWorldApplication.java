5a1d95f1e5114f353f050c3e93a1b7e98be34317
pushkar <pushkar@gmail.com>
2020-05-25 17:36:10 +0530

    first commit

---
+++ src/main/java/com/infotech/app/KK001MavenSpringBootHelloWorldApplication.java
@@ -0,0 +1,14 @@
+package com.infotech.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KK001MavenSpringBootHelloWorldApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(KK001MavenSpringBootHelloWorldApplication.class, args);
+	}
+
+}
